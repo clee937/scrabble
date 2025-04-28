16af4ac3010a2f2c7348b78658b948451c437431
Cheryl Lee <clee0937@gmail.com>
2025-04-28 12:10:39 +0100

    created letter_to_points dictionary

--- scrabble.py
+++ scrabble.py
@@ -1,3 +1,8 @@
 letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
 points = [1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 4, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10]
+
+# Creates a dictionary using dictionary comprehension that has the elements of 'letters' as the keys and the elements of 'points' as the values
+letter_to_points = {key:value for key, value in zip(letters, points)}
+
+
